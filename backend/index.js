002224554856bf369abc2f4f4d33a2f80385a9d5
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-08-27 23:54:21 -0500

    Initial project

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cookieParser from "cookie-parser";
+import dotenv from 'dotenv';
+import { connectDB } from './db/connectDB.js';
+
+import authRoutes from './routes/auth.route.js';
+
+dotenv.config();
+
+const app = express()
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log('Server is running on port:', PORT)
+});
