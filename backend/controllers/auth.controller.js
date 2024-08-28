330a3ad2c69e8e85970ef02406009798b4ff8dec
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-08-27 23:54:40 -0500

    Create Auth services

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,57 @@
+import bcryptjs from 'bcryptjs';
+
+import { User } from "../models/user.model.js";
+import { generateVerificationToken, generateTokenAndSetCookie } from '../utils/auth.js';
+
+export const signup = async (req, res) => {
+  const { email, password, name } = req.body;
+  console.log(req)
+  try {
+
+
+    if (!email || !password || !name) {
+      throw new Error('All fields are required');
+    }
+
+    const userAlreadyExists = await User.findOne({ email });
+
+    if (userAlreadyExists) {
+      return res.status(400).json({ success: false, message: 'User already exists' });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const verificationToken = generateVerificationToken();
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000
+    })
+
+    await user.save()
+
+    generateTokenAndSetCookie(res, user._id);
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      }
+    })
+
+
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+
+};
+
+export const logout = async (req, res) => {
+
+};
