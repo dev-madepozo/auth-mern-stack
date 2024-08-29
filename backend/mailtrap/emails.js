d2047a3d801619ba1b786a84e32c687c228b1e51
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-08-28 23:20:29 -0500

    Create the verify-email controller

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -18,3 +18,22 @@ export const senfVerificationEmail = async (email, verificationToken) => {
     throw new Error('Error sending verification email: ', error);
   }
 }
+
+export const sendWelcomeEmail = async (email, name) => {
+  const recipient = [{ email }]
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: "85ec4e4c-4048-4244-8788-9fa93d420628",
+      template_variables: {
+        company_info_name: 'Auth Company',
+        name
+      }
+    });
+    console.log('Welcome email sent successfully', response);
+  } catch (error) {
+    console.error('Error sending welcome email', error);
+    throw new Error('Error sending welcome email:', error);
+  }
+}
