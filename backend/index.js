e6f7e700e99aa32b9ca17fc6837b3e8717df9234
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-09-05 23:51:35 -0500

    Preparate project for production

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,13 @@ import express from 'express';
 import cookieParser from "cookie-parser";
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
+
 import { connectDB } from './db/connectDB.js';
 
 import authRoutes from './routes/auth.route.js';
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -17,6 +21,14 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  })
+}
 
 app.listen(PORT, () => {
   connectDB();
